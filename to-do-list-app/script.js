f8b7b4d7030c88553adc36ac15b9a7346b3546a3
Saurav <varmasaurav28@gmail.com>
2025-03-05 09:44:03 +1300

    Select task input field, add task button, and task list elements

---
+++ to-do-list-app/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// Select elements (Task input field , add task button and the list)
+
+const taskInput = document.querySelector(".task-input");
+const addTask = document.querySelector(".add-task-btn");
+const taskList = document.querySelector(".task-list");
