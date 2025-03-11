81c30fb83d166289ceda8810d5a2dabe35edf98a
Saurav <varmasaurav28@gmail.com>
2025-03-12 10:00:43 +1300

    Add function to create new task, rename variable from addTask to addTaskBtn for clarity

--- to-do-list-app/script.js
+++ to-do-list-app/script.js
@@ -3,5 +3,26 @@
 // Select elements (Task input field , add task button and the list)
 
 const taskInput = document.querySelector(".task-input");
-const addTask = document.querySelector(".add-task-btn");
+const addTaskBtn = document.querySelector(".add-task-btn");
 const taskList = document.querySelector(".task-list");
+
+// Function to add new task
+const addTask = () => {
+  // Get input value and remove extra spaces
+  const taskText = taskInput.value.trim();
+  // Prevent from adding empty task
+  if (taskText === "") return;
+
+  // Create a task item and add it to the list
+  const taskElement = document.createElement("li");
+  taskElement.classList.add("task-item");
+  taskList.appendChild(taskElement);
+
+  //Create span element for task text and add it to the task item
+  const taskSpan = document.createElement("span");
+  taskSpan.textContent = taskText;
+  taskElement.appendChild(taskSpan);
+
+  //Clear input field
+  taskInput.value = "";
+};
